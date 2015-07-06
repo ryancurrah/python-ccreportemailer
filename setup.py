771750cce1d9812198827dccb5ef80251c2eff20
Ryan Currah <Ryan.Currah@td.com>
2015-07-06 09:48:36 -0400

    added setup py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='python-ccreportemailer',
+    version='0.1',
+    packages=['lxml', 'requests', 'argparse'],
+    url='https://github.com/ryancurrah/python-ccreportemailer',
+    license='GNU v2',
+    author='Ryan Currah',
+    author_email='ryan@currah.ca',
+    description='Easily email Cloud Cruiser reports with this Python tool.'
+)
